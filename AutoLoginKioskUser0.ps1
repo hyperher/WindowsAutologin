404a8fa76813165c2654d779cddc1bd7238da53f
hyperher <mihernan@live.com>
2026-04-22 15:49:06 +0200

    Add files via upload

---
+++ AutoLoginKioskUser0.ps1
@@ -0,0 +1,36 @@
+$Username = "koiskuser0"
+$key = ""
+$folderPath = "C:\Program Files\UEMutil\logs"
+
+# Ensure both directories exist
+if (-not (Test-Path $path)) {
+    New-Item -Path $path -ItemType Directory | Out-Null
+}
+
+$transcriptPath = "C:\Program Files\UEMutil\logs\Adiautologin.log"
+Start-Transcript -Path $transcriptPath -Append
+
+# Check if user exists, if not, create it
+$user = Get-LocalUser -Name $Username -ErrorAction SilentlyContinue
+if (-not $user) {
+    if ([string]::IsNullOrEmpty($key)) {
+        $securePassword = [System.Security.SecureString]::new()
+    } else {
+        $securePassword = ConvertTo-SecureString $key -AsPlainText -Force
+    }
+    New-LocalUser -Name $Username -Password $securePassword -AccountNeverExpires -PasswordNeverExpires
+
+ 
+}
+
+$RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+
+If ((Get-Item -Path $RegistryPath).GetValue('ForceAutoLogon') -ne '1') {Set-ItemProperty -Path $RegistryPath -Name 'ForceAutoLogon' -Value 1}
+
+If ((Get-Item -Path $RegistryPath).GetValue('AutoAdminLogon') -ne '1') {Set-ItemProperty -Path $RegistryPath -Name 'AutoAdminLogon' -Value 1}
+
+If ((Get-Item -Path $RegistryPath).GetValue('DefaultUsername') -ne $Username) {Set-ItemProperty -Path $RegistryPath -Name 'DefaultUsername' -Value $Username}
+
+If ((Get-Item -Path $RegistryPath).GetValue('DefaultPassword') -ne $key) {Set-ItemProperty -Path $RegistryPath -Name 'DefaultPassword' -Value $key}
+
+Stop-Transcript
